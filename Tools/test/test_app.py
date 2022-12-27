1deb9fdcfc07c66be92db8a1d62e197b0e9a3ddc
matinmh <matinmoharami@yahoo.com>
2022-12-27 12:50:55 +0330

    rename

--- Tools/test/test_app.py
+++
@@ -1,7 +0,0 @@
-from sensitive_data import passwd
-import hashlib
-
-if hashlib.md5(passwd.encode('utf-8')).hexdigest() == 'e2fe6f800058d3f57fd5f206dfbed056':
-    print('pass is correct')
-else:
-    print('pass is incorrect')
